c88ec18b9a1f2592aa9b8efce83dd7edc89ae771
ryokichi <heitor.nakamura@hotmail.com>
2019-03-22 19:24:55 -0300

    base movimentacao

---
+++ faryDragon/src/Scripts/globais.gd
@@ -0,0 +1,17 @@
+extends Node
+var gravidade = 40
+var vel_horizontal = 100
+var vel_terminal = Vector2(300, 240)
+
+
+func _ready():
+	pass
+	
+func definir_parametros_globais():
+	#Carregar paremetros do banco de dados
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
